c6035daba67f813e9f33390127afef0df4a4f5a9
maroua199525 <2646@holbertonstudents.com>
2024-03-15 17:04:17 +0100

    add tasks to test

--- cybersecurity_basics/0x03_cryptography_basics/1-sha256.sh
+++ cybersecurity_basics/0x03_cryptography_basics/1-sha256.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo $(echo -n "$1"| sha256sum) > 1_hash.txt
+echo $(echo -n "$1"| sha256sum) > 1_hash.txt | cat 1_hash.txt
---
+++ cybersecurity_basics/0x03_cryptography_basics/maroua.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo -e -n "42f749ade7f9e195bf475f37a44cafcb\nfc5e038d38a57032085441e7fe7010b0\n18f1072de45420e57fd22ee5bd59df9e" > hash.txt
