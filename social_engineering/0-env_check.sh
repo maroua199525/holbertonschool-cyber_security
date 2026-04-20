2be4d7f72bcf938f94e86af0c390ac18bbf99f82
maroua199525 <2646@holbertonstudents.com>
2026-04-20 09:32:05 +0100

    add to test

--- social_engineering/0-env_check.sh
+++ social_engineering/0-env_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tools=("setoolkit" "apache2" "zip" "sendmail")
+tools=("setoolkit" "zip" "apache2" "sendmail")
 
 for tool in "${tools[@]}"; do
     if command -v "$tool" &>/dev/null || dpkg -l "$tool" &>/dev/null 2>&1; then
