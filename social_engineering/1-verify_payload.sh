b1f2c49d5169e165b0125ed37619b55d547f84ef
maroua199525 <2646@holbertonstudents.com>
2026-04-20 10:49:03 +0100

    add to test

--- social_engineering/1-verify_payload.sh
+++ social_engineering/1-verify_payload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -f "/root/.set/payload.exe" ]; then
-    echo "[+] SET Payload generated successfully"
+    echo "SET Payload generated successfully"
 else
-    echo "[-] Error: Payload not found"
+    echo "Error: Payload not found."
 fi
